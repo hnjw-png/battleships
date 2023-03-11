d7807daf20e82a79c6f68bf7008c48370288de95
Holly Williams <hollyyeh12@gmail.com>
2023-03-11 16:39:57 +0000

    edit gameborard

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Your code goes here
+from random import randint
+
+print("Enter your name:")
+x = input()
+print("Hello, lets play Battleships!" + x)
+
+class board:
+def __init__(self, board):
+    self.board = board
+
+
+
+
+
+
+
+
+
