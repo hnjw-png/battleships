11b472d91f985acb9dcd7c389b2424a9c4fa85f5
Holly Williams <hollyyeh12@gmail.com>
2023-03-12 19:54:24 +0000

    add to board function

--- run.py
+++ run.py
@@ -1,19 +1,39 @@
-# Your code goes here
 from random import randint
-
+#hello message to player
 print("Enter your name:")
 x = input()
 print("Hello, lets play Battleships!" + x)
 
-class board:
-def __init__(self, board):
+#setting board rows and columns
+class Board:
+   def __init__(self, size, num_ships):
+    self.size = size
     self.board = board
+    self.num_ships = num_ships
 
+   def (self.size) :
+        for row in range(5):
+        for col in range(5):
+            board(size)
 
+#setting the random placement of ships
 
+    def random_row(self, num_ships):
+    return randint(0, len(Board) - 1 )
 
+    def random_col(self, num_ships):
+    return randint(0, len(Board[0]) - 1 )
 
+ship_row = rand_row(Board)
+ship_col = random_col(Board)
 
+def game():
+    print_board(Board)
 
-
+#asking the player to guess a spot
+print("Type you guess, between 0-4")
+x = input()
+print("You typed:" + x)
+if:
+   
 
