87880daf59d99484beb40530ed580f055ac67c38
Holly Williams <hollyyeh12@gmail.com>
2023-03-14 15:32:02 +0000

    print board and other edits

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
 from random import randint
+player = 0
+computer = 0
+
 #hello message to player
 print("Enter your name:")
 x = input()
@@ -11,10 +14,21 @@ class Board:
     self.board = board
     self.num_ships = num_ships
 
-   def (self.size) :
-        for row in range(5):
-        for col in range(5):
-            board(size)
+def board in range:
+    for row in range(5):
+    for col in range(5):
+            #or for x in range(5)??
+    board.append(["_"] * 5)
+
+    
+#assign letters and numbers to board
+    def print_board(Board):
+         for row in range(5):
+         for col in range(5):
+        print(" ", " ".join("12345"))
+        for letter, row in zip("ABCDE", Board):
+            print(letter, " ".join(row))
+   
 
 #setting the random placement of ships
 
@@ -27,13 +41,37 @@ class Board:
 ship_row = rand_row(Board)
 ship_col = random_col(Board)
 
+print(ship_row)
+print(ship_col)
+
 def game():
     print_board(Board)
 
 #asking the player to guess a spot
-print("Type you guess, between 0-4")
+print("Type you guess, between 1-5")
 x = input()
 print("You typed:" + x)
-if:
-   
 
+print("Guess a letter")
+x =  input()
+print("You typed: " + x)
+   
+print(Board)
+
+#seeing if you sunk the oppenants battleship
+#control that the player cannot chose outside of the board
+#print x for a guess
+if ship_row == guess_row and ship_col == guess_col:
+    print("Well done! You sunk my Battleship!")
+else:
+    if guess_row not in range(5) or guess_col not in range(5):
+    print("You missed the board!")
+    elif Board[guess_row][guess_col] == "X"
+    print_board(Board)
+
+#restart game upon first ship being sunk
+if ship_row == guess_row and ship_col == guess_col:
+    start()
+
+
+#add more ships to the board
