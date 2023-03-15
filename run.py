0b01204d5b36f530a11449600cc995a9ee049ea6
Holly Williams <hollyyeh12@gmail.com>
2023-03-15 13:05:56 +0000

    add variables for hitting and missing boats.

--- run.py
+++ run.py
@@ -1,75 +1,47 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 from random import randint
-#hello message to player
+
+# hello message to player
 print("Enter your name:")
 x = input()
-print("Hello, lets play Battleships!" + x)
-
-#setting board rows and columns
-class board:
-   def __init__(self, size, num_ships):
-    self.size = size
-    self.board = board
-    self.shipone_col = shipone_col
-    self.shiptwo_col = shiptwo_col
-    self.shipone_row = shipone_row
-    self.shiptwo_row = shiptwo_row
-    self.shipone_won = shipone_won
-    self.shiptwo_won = shiptwo_won
+print("Hello, lets play Battleships " + x)
 
 
-board(size) = []  
+board = []
 
-for o in range(5):
-    board.append(["0"] * 5 )
+for x in range(5):
+    board.append(["0"] * 5)
 
-def board(board):
+
+def print_board(board):
     for row in board:
         print(" ".join(row))
 
-def print_board(board):
-        print(" ", " ".join("12345"))
-        for letter, row in zip("ABCDE", Board):
-            print(letter, " ".join(row))
 
 print_board(board)
 
 
-#randomly find ship coordinates
+# randomly find ship coordinates
 def random_row(board):
-    return randrange(0, len(board))
+    return randint(0, len(board))
 
-def random_row(board):
-    return randrange(0, len(board))
+def random_col(board):
+    return randint(0, len(board))
 
 
-#random coordinates to ship one
+# random coordinates to ship one
 
-shipone_col = random_row(board)
+shipone_row = random_row(board)
 shipone_col = random_col(board)
 
-#randow coordinates for ship two
+
+
+# randow coordinates for ship two
 
 for i in range(6):
     shiptwo_row = random_row(board)
-    shiptwo_col = randow_col(board)
+    shiptwo_col = random_col(board)
+    if shiptwo_row != shipone_row and shiptwo_col != shipone_col:
+        break
 
 
 shipone_won = False
@@ -77,40 +49,40 @@ shiptwo_won = False
 
 for turn in range(6):
 
-#receive the players guess
+# receive the players guess
 
-    print ("Round #" + str (turn + 1))
+    print("Round #" + str (turn + 1))
     guess_row = int(input("Guess Row:"))
     guess_col = int(input("Guess Col:"))
 
-#this will check if the is users guesses match ships
+# this will check if the is users guesses match ships
 
     if (guess_row == shipone_row and guess_col == shipone_col and shipone_won == False):
         print("You have taken one ship, one  to go!")
         shipone_won = True
 
-    #set the space to x, so we know where the last boat was hit
+    # set the space to p, so we know where the last boat was hit
 
-        board[guess_col][guess_row] = "x"
+        board[guess_col][guess_row] = "p"
 
-        if shipone_won and shiptwo_won == True:
+        if shipone_won and shiptwo_won is True:
             print("Congratulations, you have taken down both ships")
             break
 
 
-    elif(guess_row == shiptwo_row and guess_col == shiptwo_col and shiptwo_won == False):
+    elif(guess_row = shiptwo_row and guess_col == shiptwo_col and shiptwo_won == False):
         print("Congrats! You have taken down one ship")
-        board[guess_row][guess_col] = "x"
+        board[guess_row][guess_col] = "p"
         if shiptwo_won == True and shiptwo_won == True:
             print("Congrats, You have taken down both ships!")
 
 
     else:
         if turn == 5:
-            print "You did not sink all the battleships, GAME OVER!"
+            print("You did not sink all the battleships, GAME OVER!")
 
-        elif([guess_row][guess_col] == "x")
-            print "Oops, you hit the same place!"
+        elif (guess_row > shipone_row or )
+
 
 
 
