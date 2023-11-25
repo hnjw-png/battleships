08c0db71de21003f7a7dfb760ae708d011aa44f8
Holly Williams <hollyyeh12@gmail.com>
2023-11-25 18:13:50 +0000

    add run game function

--- run.py
+++ run.py
@@ -1,5 +1,4 @@
 from random import randint
-
 # hello message to player
 print("Welcome to Battleships, this is a one player game. You have 5 turns, and guesses must be between 0-5")
 print("Enter your name:")
@@ -72,11 +71,11 @@ def RunGame():
     while turns > 0:
         Board.print_board(user_guess_board)
         user_x_row, user_y_column = Ship.get_user_input(object)
-        while user_guess_board.board[user_x_row][user_y_column] == "-" or user_guess_board.
-        board[user_x_row][user_x_row] == "p":
-          print("You have already chose this spot, please pick another")
-          user_x_row, user_y_column == Ship.get_user_input(object)
-        if computer_board.board[user_x_row][user_y_column] == "p":
+        while user_guess_board.board[user_x_row][user_y_column] == "-" or user_guess_board:
+         board[user_x_row][user_x_row] == "p":
+           print("You have already chose this spot, please pick another")
+           user_x_row, user_y_column == Ship.get_user_input(object)
+        if computer_board.board[user_x_row][user_y_column] == "p" :
             print("You managed to find one of my ships, what a hit!")
             user_guess_board.board[user_x_row][user_y_column] = "p"
         else:
@@ -93,55 +92,9 @@ def RunGame():
                 Board.print_board(user_guess_board)
                 break
 
+if __name__ == '__main__':
+    RunGame()
 
 
 
 
-
-# randow coordinates for ship two
-
-for i in range(6):
-    shiptwo_row = random_row(board)
-    shiptwo_col = random_col(board)
-    if shiptwo_row != shipone_row and shiptwo_col != shipone_col:
-        break
-
-
-shipone_won = False
-shiptwo_won = False
-
-for turn in range(6):
-
-# receive the players guess
-
-    print("Round #" + str(turn + 1))
-    guess_row = int(input("Guess Row:"))
-    guess_col = int(input("Guess Col:"))
-
-# this will check if the is users guesses match ships
-
-    if (guess_row == shipone_row and guess_col == shipone_col and shipone_won == False):
-        print("You have taken one ship, one to go!")
-        shipone_won = True
-
-    # set the space to p, so we know where the last boat was hit
-
-        board[guess_col][guess_row] = "p"
-
-        if shipone_won and shiptwo_won is True:
-            print("Congratulations, you have taken down both ships")
-            break
-
-
-        elif(guess_row == shiptwo_row and guess_col == shiptwo_col and shiptwo_won == False):
-            print("Congrats! You have taken down one ship")
-        board[guess_row][guess_col] = "p"
-        if shiptwo_won == True and shiptwo_won == True:
-            print("Congrats, You have taken down both ships!")
-            break
-
-
-    else:
-        if turn == 5:
-            print("You did not sink all the battleships, GAME OVER!")
-            break
