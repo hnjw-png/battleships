a20bf87c951f22762332206c7595a97079eafe20
Holly Williams <hollyyeh12@gmail.com>
2023-12-01 20:09:14 +0000

    edit count ships function

--- run.py
+++ run.py
@@ -40,28 +40,31 @@ class Ship:
 #ask for the users input, and react if not correct     
     def get_user_input(self):
       try:
-        x_row = input("Type in the Ship row number:")
-        while x_row not in '1234567':
-            print("not in the right place.")
-            x_row = input("Type in row of ship again:")
+          x_row = int(input("Type in the Ship row number:")) - 1
+          while x_row not in range(0, 7):  
+              print("Not in the right place.")
+              x_row = int(input("Type in row of ship again:")) - 1
 
-        y_column = input("Type in the Ship column NUMBER:")
-        while y_column not in '1234567':
-            print("not in the right place.")
-            y_column = input("Type in column of ship again:")
-        return int(x_row) -1, (y_column)
-      except ValueError and KeyError:
-        print("not valid")
-        return self.get_user_input()
+          y_column = int(input("Type in the Ship column NUMBER:")) - 1
+          while y_column not in range(0, 7):  
+              print("Not in the right place.")
+              y_column = int(input("Type in column of ship again:")) - 1
+
+          return x_row, y_column
+      except ValueError:
+          print("Invalid input. Please enter a valid row and column.")
+          return self.get_user_input()
+
 
 
 # randomly find ship coordinates
     def count_found_ships(self):
       found_ships = 0
       for row in self.board:
-          for column in row == 'p':
-              found_ships += 1
-              return found_ships
+          for column in row:
+              if column == 'p':
+                  found_ships += 1
+      return found_ships
 
 
 def RunGame():
@@ -75,9 +78,9 @@ def RunGame():
         #what did the user choose?
         user_x_row, user_y_column = Ship.get_user_input(object)
         #stop the player picking the same place twice
-        while user_guess_board.board(user_x_row)(user_y_column) == "-" or user_guess_board.board(user_x_row)(user_y_column) == "p" :
-          print("You have already chose this spot, please pick another")
-        user_x_row, user_y_column == Ship.get_user_input(object)
+        while user_guess_board.board[user_x_row][user_y_column] == "-" or user_guess_board.board[user_x_row][user_y_column] == "p":
+          print("You have already chosen this spot, please pick another")
+          user_x_row, user_y_column = Ship.get_user_input(object)
           #is it a hit or miss?
         if computer_board.board[user_x_row][user_y_column] == "p" :
           print("You managed to find one of my ships, what a hit!")
