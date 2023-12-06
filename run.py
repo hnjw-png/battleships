44f241016056e3c879b017baf56f5e5ea660b3fe
Holly Williams <hollyyeh12@gmail.com>
2023-12-06 11:46:24 +0000

    final test, edit counter to interger.

--- run.py
+++ run.py
@@ -100,7 +100,7 @@ def RunGame():
             print("You did not find my ship!")
             user_guess_board.board[user_x_row][user_y_column] = "-"
         # are all ships found?
-        if Ship.count_found_ships(user_guess_board) == "5":
+        if Ship.count_found_ships(user_guess_board) == 5:
             print("Well done you managed to find all the ships!")
             break
         else:
